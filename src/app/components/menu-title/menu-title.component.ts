eb2f83f18ac44ce6286e47c0c6bc1f0eb2833135
Ana Paula <apdsmenezes1809@gmail.com>
2023-10-05 21:28:41 -0300

    desacoplando os components

--- src/app/components/menu-title/menu-title.component.ts
+++ src/app/components/menu-title/menu-title.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-menu-title',
@@ -7,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
   './menu-title.responsive.component.css']
 })
 export class MenuTitleComponent implements OnInit {
-
+  @Input()
+  menuTitle:String =""
   constructor() { }
 
   ngOnInit(): void {
