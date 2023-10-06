04289d66798d8067690af9633b94cee5ca458f26
Ana Paula <apdsmenezes1809@gmail.com>
2023-10-06 00:55:55 -0300

    comitando component

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "Gato apocalíptico deixa tutora desesperada durante pós-operatório",
+    "description": "“Ele chegou com várias fraturas no membro pélvico direito. Fizemos campanhas de doação e conseguimos fazer a cirurgia dele, na Unidade Veterinária Setúbal, em Recife. Estava previsto para retirar os fixadores com 60 dias, mas quando fizemos um novo raio-x, não estava totalmente cicatrizado ainda, e o cirurgião ortopedista pediu mais 30 dias”.",
+    "photoCover": "https://www.cnnbrasil.com.br/wp-content/uploads/sites/12/2023/08/andreza-ferreira-arquivo-pessoal-e1692205832985.jpeg?w=1220&h=674&crop=1"
+  },
+  {
+    "id":"2",
+    "title": "Gato seduz internautas ao dançar",
+    "description": "Gatimho dançando, muito fofo.",
+    "photoCover": "https://fotos.amomeupet.org/uploads/fotos/660x0_1695998798_6516e34e58a32_hd.webp"
+  },
+  {
+    "id":"3",
+    "title": "Gata desperta atenção de voluntários de ONG.",
+    "description": "Gatinha pedindo para ser adotada.",
+    "photoCover": "https://fotos.amomeupet.org/uploads/fotos/660x0_1695347417_650cf2d95ec2c_hd.webp"
+  },
+  {
+    "id":"4",
+    "title": "Gato de abrigo usa roupas fofas todos os dias.",
+    "description": "Alguém nota esse gato e adota ele por favor.",
+    "photoCover": "https://fotos.amomeupet.org/uploads/fotos/660x0_1695323008_650c938074267_hd.webp"
+  }
+]
